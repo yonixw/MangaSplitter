5caafb0487212ba7579f1ac70a6c3ec16ac90c4c
yonixw <yonixw@gmail.com>
2018-05-25 02:46:08 +0300

    more gui stuff

--- MangaSplitter/FrmMain.Designer.cs
+++ MangaSplitter/FrmMain.Designer.cs
@@ -31,27 +31,26 @@
             this.button2 = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
-            this.lblPath = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.diagFolder = new System.Windows.Forms.SaveFileDialog();
             this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
-            this.lblTarget = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.lblPath = new System.Windows.Forms.TextBox();
+            this.lblTarget = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(506, 563);
+            this.button2.Location = new System.Drawing.Point(380, 558);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(140, 37);
             this.button2.TabIndex = 11;
@@ -61,7 +60,7 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(120, 142);
+            this.numericUpDown1.Location = new System.Drawing.Point(189, 141);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             15000,
             0,
@@ -81,21 +80,13 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 144);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.Size = new System.Drawing.Size(171, 20);
             this.label2.TabIndex = 9;
-            this.label2.Text = "Width Cutoff:";
-            // 
-            // lblPath
-            // 
-            this.lblPath.AutoSize = true;
-            this.lblPath.Location = new System.Drawing.Point(20, 51);
-            this.lblPath.Name = "lblPath";
-            this.lblPath.Size = new System.Drawing.Size(0, 20);
-            this.lblPath.TabIndex = 8;
+            this.label2.Text = "Single page max width:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(209, 17);
+            this.button1.Location = new System.Drawing.Point(398, 49);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(107, 33);
             this.button1.TabIndex = 7;
@@ -108,9 +99,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 20);
+            this.label1.Size = new System.Drawing.Size(391, 20);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Choose Source Directory:";
+            this.label1.Text = "Choose Source Directory (Subfolders are included): [-i]";
             // 
             // diagFolder
             // 
@@ -121,17 +112,9 @@
             this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
             this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
             // 
-            // lblTarget
-            // 
-            this.lblTarget.AutoSize = true;
-            this.lblTarget.Location = new System.Drawing.Point(21, 115);
-            this.lblTarget.Name = "lblTarget";
-            this.lblTarget.Size = new System.Drawing.Size(0, 20);
-            this.lblTarget.TabIndex = 14;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(209, 81);
+            this.button3.Location = new System.Drawing.Point(397, 105);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(107, 33);
             this.button3.TabIndex = 13;
@@ -142,38 +125,16 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 87);
+            this.label4.Location = new System.Drawing.Point(13, 85);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(185, 20);
+            this.label4.Size = new System.Drawing.Size(211, 20);
             this.label4.TabIndex = 12;
-            this.label4.Text = "Choose Target Directory:";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox2.Image = global::MangaSplitter.Properties.Resources.Booklet;
-            this.pictureBox2.Location = new System.Drawing.Point(190, 228);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(165, 376);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox2.TabIndex = 16;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Image = global::MangaSplitter.Properties.Resources.Duplex;
-            this.pictureBox1.Location = new System.Drawing.Point(17, 228);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(158, 376);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
+            this.label4.Text = "Choose Target Directory: [-d]";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 191);
+            this.label3.Location = new System.Drawing.Point(12, 176);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 20);
             this.label3.TabIndex = 17;
@@ -182,42 +143,78 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(136, 191);
+            this.radioButton1.Location = new System.Drawing.Point(247, 176);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(76, 24);
+            this.radioButton1.Size = new System.Drawing.Size(102, 24);
             this.radioButton1.TabIndex = 18;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Duplex";
+            this.radioButton1.Text = "Duplex [-d]";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(230, 191);
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(132, 176);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(81, 24);
+            this.radioButton2.Size = new System.Drawing.Size(107, 24);
             this.radioButton2.TabIndex = 19;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Booklet";
+            this.radioButton2.Text = "Booklet [-b]";
             this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // lblPath
+            // 
+            this.lblPath.Location = new System.Drawing.Point(12, 52);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.ReadOnly = true;
+            this.lblPath.Size = new System.Drawing.Size(380, 26);
+            this.lblPath.TabIndex = 20;
+            // 
+            // lblTarget
+            // 
+            this.lblTarget.Location = new System.Drawing.Point(12, 109);
+            this.lblTarget.Name = "lblTarget";
+            this.lblTarget.ReadOnly = true;
+            this.lblTarget.Size = new System.Drawing.Size(380, 26);
+            this.lblTarget.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(315, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "(px) [-p]";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::MangaSplitter.Properties.Resources.info;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 206);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 346);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 612);
+            this.ClientSize = new System.Drawing.Size(528, 603);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTarget);
+            this.Controls.Add(this.lblPath);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.lblTarget);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.lblPath);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
@@ -226,10 +223,9 @@
             this.MaximizeBox = false;
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Main config";
+            this.Text = "MangaSplitter GUI";
             this.Load += new System.EventHandler(this.FrmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -241,18 +237,18 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label lblPath;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.SaveFileDialog diagFolder;
         private System.ComponentModel.BackgroundWorker backgroundWorker;
-        private System.Windows.Forms.Label lblTarget;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox lblPath;
+        private System.Windows.Forms.TextBox lblTarget;
+        private System.Windows.Forms.Label label5;
     }
 }
